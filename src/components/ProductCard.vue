c6f5a9787351bc6c50d203f0f701fa3fcc3103e6
Wenyu <Judy30058@gmail.com>
2025-05-10 21:09:11 +0800

    feat: display product name,price,inventory

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,5 +1,9 @@
 <script setup></script>
 
-<template></template>
+<template>
+  <p>{{ product.name }}</p>
+  <p>{{ product.price }}</p>
+  <p>{{ product.product.inventory }}</p>
+</template>
 
 <style scoped></style>
