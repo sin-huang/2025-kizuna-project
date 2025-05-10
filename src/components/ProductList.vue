1604392d21db98db13ae55ee66c986f664e60d22
Wenyu <Judy30058@gmail.com>
2025-05-10 20:59:58 +0800

    feat: ProductList has data bind

---
+++ src/components/ProductList.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useProductStore } from "@/stores/products.js";
+import ProductCard from "@/components/ProductCard.vue";
+const productStore = useProductStore();
+</script>
+
+<template>
+  <ProductCard
+    v-for="product in products"
+    :key="product.id"
+    :name="product.name"
+    :price="product.price"
+    :inventory="product.inventory"
+  />
+</template>
+
+<style scoped></style>
