c0560f12918c6246a211553cb9d1d90d88f9e171
Wenyu <Judy30058@gmail.com>
2025-05-11 09:43:16 +0800

    feat:add product.js logic

--- src/stores/products.js
+++ src/stores/products.js
@@ -8,5 +8,27 @@ export const useProductStore = defineStore("products", () => {
     { id: 3, name: "口紅", price: "200", inventory: 7 },
   ]);
 
-  return { products }; //記得return
+  //取得所有可以購買的商品(庫存大於0) 記得計算要加computed!!!!
+  const avaliableProduct = computed(() => {
+    return products.value.filter((product) => product.inventory > 0);
+  });
+
+  //減少庫存
+  const decreaseInventory = (productId) => {
+    const product = products.value.find((product) => product.id === productId);
+
+    if (product && product.inventory > 0) {
+      product.inventory--;
+    }
+  };
+
+  //增加庫存
+  const increaseInventory = (productId) => {
+    const product = products.value.find((product) => product.id === productId);
+
+    if (product) {
+      product.inventory++;
+    }
+  };
+  return { products, avaliableProduct, decreaseInventory, increaseInventory }; //記得return
 });
