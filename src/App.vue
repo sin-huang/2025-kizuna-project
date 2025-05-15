d7f678e5bcd97bd9ef471c16e936bb7adc5889ff
Wenyu <Judy30058@gmail.com>
2025-05-15 15:00:25 +0800

    feat: add css in App.vue
    fix: the package-lock.json conflicts

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,15 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Product</RouterLink>
-      <RouterLink to="/cart">Cart</RouterLink>
+  <header class="bg-blue-900 text-white shadow-md">
+    <nav class="flex justify-start">
+      <RouterLink to="/" class="p-6 text-lg hover:text-gray-300 transition">Product</RouterLink>
+      <RouterLink to="/cart" class="p-6 text-lg hover:text-gray-300 transition">Cart</RouterLink>
     </nav>
   </header>
-
-  <RouterView />
+  <main class="max-w-[1000px] mx-auto mt-24 ">
+    <RouterView />
+  </main>
 </template>
 
 <style scoped></style>
