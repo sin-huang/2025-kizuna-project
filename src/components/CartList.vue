13783f92da8dc2f77ae4d21ee3ffef128f792033
Wenyu <Judy30058@gmail.com>
2025-05-15 15:02:37 +0800

    feat: add css in CartList.vue

--- src/components/CartList.vue
+++ src/components/CartList.vue
@@ -2,11 +2,34 @@
 import CartCard from "@/components/CartCard.vue";
 import { useCartStore } from "@/stores/cart.js";
 const cartStore = useCartStore();
-const { cartItems } = cartStore;
+//解構出來的東西不是響應式
 </script>
 
 <template>
-  <CartCard v-for="item in cartItems" :key="item.id" :item="item" />
+  <h1 class="text-2xl">購物車</h1>
+  <br />
+  <div class="grid grid-cols-5 border-b">
+    <p class="col-span-2">商品資訊</p>
+    <p>價格 (含稅)</p>
+    <p>數量</p>
+  </div>
+  <br />
+  <div v-if="cartStore.cartItems.length > 0">
+    <CartCard v-for="item in cartStore.cartItems" :key="item.id" :item="item" />
+    <br />
+    <div class="text-right">
+      <button
+        @click="cartStore.clearCart()"
+        class="bg-blue-600 text-white w-fit px-4 py-1 rounded hover:bg-blue-700"
+      >
+        清空購物車
+      </button>
+      <p>總計: {{ cartStore.totalPrice }} 元</p>
+    </div>
+  </div>
+  <div v-else>
+    <p>購物車為空</p>
+  </div>
 </template>
 
 <style scoped></style>
