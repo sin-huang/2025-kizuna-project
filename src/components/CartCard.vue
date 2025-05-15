f4618e3890eca194ced1ae4c4cffb2c8e03dda52
Wenyu <Judy30058@gmail.com>
2025-05-15 15:01:27 +0800

    feat: handleQuantityChange

--- src/components/CartCard.vue
+++ src/components/CartCard.vue
@@ -1,8 +1,17 @@
 <script setup>
 import { useCartStore } from "@/stores/cart.js";
+import { useProductStore } from "@/stores/products.js";
 const cartStore = useCartStore();
-const { cartItems } = cartStore;
-//傳資料:三部曲
+const productStore = useProductStore();
+// 傳資料
+// const { cartItems, totalPrice, totalItems } = storeToRefs(cartStore); 這邊不會用到這些資訊，函式不能寫入storeToRefs?
+
+const handleQuantityChange = (itemId, newQuantity) => {
+  if (newQuantity < 1) {
+    newQuantity = 1; // 確保最小值為 1
+  }
+  cartStore.updateQuantity(itemId, newQuantity);
+};
 
 defineProps({
   item: {
@@ -13,9 +22,25 @@ defineProps({
 </script>
 
 <template>
-  <p>商品: {{ item.name }}</p>
-  <p>價格: {{ item.price }}</p>
-  <p>數量: {{ item.quantity }}</p>
+  <div class="grid grid-cols-5 items-center">
+    <div class="col-span-2">
+      <img src="" alt="" />
+      <p>商品: {{ item.name }}</p>
+    </div>
+    <p>價格: {{ item.price }}</p>
+    <input
+      type="number"
+      :value="item.quantity"
+      @change="handleQuantityChange(item.id, $event.target.valueAsNumber)"
+    />
+    <button
+      @click="cartStore.removeFromCart(item.id)"
+      class="bg-blue-600 text-white w-fit px-4 py-1 rounded hover:bg-blue-700 justify-self-end"
+    >
+      移除
+    </button>
+    <br />
+  </div>
 </template>
 
 <style scoped></style>
