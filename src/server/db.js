f5db7c9e264debbfb2bb248e0a4c10cd490f93cf
melody <melody880306@gmail.com>
2025-05-19 13:18:18 +0800

    add: check the database connect?

--- src/server/db.js
+++ src/server/db.js
@@ -8,4 +8,13 @@ const pool  = new Pool({
     port: '5432'
 });
 
+// 確認是否連線成功
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error('資料庫連線失敗', err);
+    } else {
+        console.log('資料庫連線成功', res.rows);
+    }
+});
+
 export default pool;
