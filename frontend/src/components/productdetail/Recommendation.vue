fd3f4ae2349f70bd07031550bd9c7da6eec378bc
Wenyu <Judy30058@gmail.com>
2025-05-25 11:53:30 +0800

    feat: add style to recommendation.vue

--- frontend/src/components/productdetail/Recommendation.vue
+++ frontend/src/components/productdetail/Recommendation.vue
@@ -1,11 +1,17 @@
 <script setup>
 import { useProductStore } from "@/stores/products.js";
-import ProductCard from "@/components/productdetail/ProductCard.vue"
+import ProductCard from "@/components/productdetail/ProductCard.vue";
 const productStore = useProductStore();
 const { products } = productStore;
 </script>
 <template>
-	<p>-商品資訊-</p>
-	<ProductCard v-for="product in products" :key="product.id"/>
+  <p class="text-center text-lg font-semibold text-gray-800 mt-4 mb-2">更多推薦</p>
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <ProductCard
+      v-for="product in products"
+      :key="product.id"
+      :product="product"
+    />
+  </div>
 </template>
 <style></style>
