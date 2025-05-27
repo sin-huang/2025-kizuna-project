d98b88f1476682faacbf01ed11add5e1b9e76134
HeidiSiao <heidi.siao@gmail.com>
2025-05-27 22:14:22 +0800

    fix: redirect page after successful registration

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,8 +9,7 @@ import ProfileView from "@/views/ProfileView.vue";
 import ChatRoomView from "@/views/ChatRoomView.vue";
 import MatchView from "@/views/MatchView.vue";
 import ActivityList from "@/components/ActivityList.vue";
-import EditProfileView from "../views/EditProfileView.vue";
-// import TestView from "../views/TestView.vue";
+import EditProfileView from "@/views/EditProfileView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
