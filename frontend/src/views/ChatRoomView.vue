df9154d6347ab4a1cde03fcdf079482bd6258e90
melody <melody880306@gmail.com>
2025-05-28 04:12:33 +0800

    fix: change SQL to the drizzle-orm

--- frontend/src/views/ChatRoomView.vue
+++ frontend/src/views/ChatRoomView.vue
@@ -4,20 +4,21 @@ import { io } from "socket.io-client";
 import { useUserStore } from "@/stores/user.js";
 import { userChatStore } from "@/stores/chat.js";
 import { watch } from "vue";
-import MessageBubble from "@/components/MessageBubble.vue"
+import MessageBubble from "@/components/MessageBubble.vue";
 
 // 注意 : socket 預設是前端瀏覽器網址(localhost:5173)
 // 這樣會連不上後端 localhost:3000 所以一定要指定URL
 const socket = io("http://localhost:3000");
 // 這次輸入的內容
 const message = ref("");
-const messages = ref([]);
+// const messages = ref([]);
 const userStore = useUserStore();
 const chatStore = userChatStore();
 const roomId = 1;
 
 // 訊息接收處理
 function handleIncomingMessage(msg) {
+  // console.log(msg); // msg 會是一個物件
   chatStore.addMessage(msg);
 }
 
@@ -25,8 +26,9 @@ function handleIncomingMessage(msg) {
 onMounted(() => {
   socket.emit("joinRoom", roomId);
   console.log("✅ 加入聊天室 Room", roomId);
-  // 核心: 監聽 chatMessage 動作 
-  // 只綁定一次!!! (要不然傳一個訊息 會跳出多個)  
+  // 核心: 監聽 chatMessage 動作
+  // 只綁定一次!!! (要不然傳一個訊息 會跳出多個)
+  // socket.on 是接住後端 emit("什麼事件",...) 的前端監聽器
   socket.on("chatMessage", handleIncomingMessage);
 });
 
@@ -37,7 +39,7 @@ onBeforeUnmount(() => {
 
 function sendMessage() {
   if (!message.value.trim()) return;
-  // debug   
+  // debug
   console.log(message.value);
   socket.emit("chatMessage", {
     roomId,
@@ -47,39 +49,37 @@ function sendMessage() {
   message.value = "";
 }
 
-function msgClass(msg) {
-  // 判斷到底是誰傳的 
-  console.log(msg.sender_id);
-  return parseInt(msg.sender_id) === parseInt(userStore.username) ? "text-right" : "text-left";
-}
-
-watch(()=>userStore.userId,(val)=>{
-    console.log("userId為:",val);
-})
-
+watch(
+  () => userStore.userId,
+  (val) => {
+    console.log("userId為:", val);
+  }
+);
 </script>
 
 <template>
   <div class="flex flex-col h-[50vh]">
-    <div class="bg-blue-600 text-white p-4 text-lg font-bold">聊天室</div>
-    <div class="flex-1 overflow-y-auto p-4 space-y-2 bg-gray-50">
+    <div class="p-4 text-lg font-bold text-white bg-blue-600">
+      聊天室
+    </div>
+    <div class="flex-1 p-4 space-y-2 overflow-y-auto bg-gray-50">
       <MessageBubble
-        v-for="msg in chatStore.messages"
-        :key="msg.id"
+        v-for="(msg, index) in chatStore.messages"
+        :key="index"
         :msg="msg"
-        :selfId="userStore.userId"
+        :self-id="userStore.userId"
       />
     </div>
-    <div class="p-4 flex border-t">
+    <div class="flex p-4 border-t">
       <input
         v-model="message"
-        class="flex-1 border rounded px-3 py-2 mr-2"
+        class="flex-1 px-3 py-2 mr-2 border rounded"
         placeholder="輸入訊息"
         @keyup.enter="sendMessage"
-      />
+      >
       <button
+        class="px-4 py-2 text-white bg-blue-500 rounded"
         @click="sendMessage"
-        class="bg-blue-500 text-white px-4 py-2 rounded"
       >
         發送
       </button>
