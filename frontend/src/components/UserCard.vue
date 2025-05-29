18c6dd38e972126b46302b351dd69fde9f30cffb
HeidiSiao <heidi.siao@gmail.com>
2025-05-29 13:36:07 +0800

    feat :Implement swipe logic and show user's info

--- frontend/src/components/UserCard.vue
+++ frontend/src/components/UserCard.vue
@@ -8,12 +8,14 @@ const users = ref([
     name: "西西",
     age: 28,
     zodiac: "雙子座",
+    location: "高雄市",
     photos: [
       "https://im.marieclaire.com.tw/s1200c675h100b0/aq/2015/04/16/201504131747183239.jpg",
       "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjZ6kIRmyQ8LFGVxKbivqcxwZWqoXBmy5ANTekmGdnDoahHUzSzPHbLQIeDuEV0L0dRjslUHBIg8kaQE4BU5xXoRBX4GBmUooaViZzQG44k8QURYiYcZNOqwRMnyXNw_8qzaLjeQywUmUM/s1600/emma-watson.jpg",
       "https://cdn.wowscreen.com.tw/uploadfile/202210/goods_028263_373336.png",
     ],
     bio: "我是一個熱愛旅遊的人，喜歡挑戰新事物。",
+    mbti: "INTJ",
     interests: ["攝影", "爬山", "音樂"],
     intro: "嗨，我是西西，期待認識新朋友。",
   },
@@ -21,6 +23,7 @@ const users = ref([
     id: 2,
     name: "小花",
     age: 25,
+    location: "台中市",
     zodiac: "處女座",
     photos: [
       "https://image1.gamme.com.tw/news2/2022/10/07/rJyWnaOWkaOcr6c.jpg",
@@ -28,6 +31,7 @@ const users = ref([
       "https://a.ksd-i.com/s/480x_86400_583f780f8611ab72028feb102bfe29f4/static.koreastardaily.com/2018-10-05/109958-662192.jpg",
     ],
     bio: "喜歡閱讀和烹飪，平常喜歡安靜的午後時光。",
+    mbti: "ENfJ",
     interests: ["閱讀", "烹飪", "瑜伽"],
     intro: "嗨，我是小花，希望找到志同道合的朋友。",
   },
@@ -35,6 +39,7 @@ const users = ref([
     id: 3,
     name: "小玉",
     age: 25,
+    location: "新竹市",
     zodiac: "處女座",
     photos: [
       "https://assets.juksy.com/files/articles/63508/58c17ded2aaf9.jpg",
@@ -42,6 +47,7 @@ const users = ref([
       "https://assets.juksy.com/files/articles/63508/58c17ea5e111d.jpg",
     ],
     bio: "安靜安靜安靜安靜安靜安靜。",
+    mbti: "ISTJ",
     interests: ["閱讀", "安靜", "安靜"],
     intro: "嗨，我是小玉，希望找到志同道合的朋友。",
   },
@@ -60,75 +66,99 @@ const prevUser = () => {
 const nextUser = () => {
   currentIndex.value = (currentIndex.value + 1) % users.value.length;
 };
+
+const isShow = ref(false);
+const infoBtnTxt = ref("Show More");
+const infoToggle = () => {
+  isShow.value = !isShow.value;
+  infoBtnTxt.value = isShow.value ? "Hide Info" : "Show More";
+};
 </script>
 
 <template>
-  <main class="bg-[#E0E0E0] p-8 flex items-center justify-around">
-    <button type="button" @click="prevUser" class="circle-wrap">
-      <div class="left-arrow transform -translate-x-[2px]"></div>
-    </button>
+  <main
+    class="bg-[#E0E0E0] px-6 pt-6 flex items-center justify-around flex-col"
+  >
+    <section class="flex items-center p-2 gap-3 mb-4">
+      <button type="button" @click="prevUser" class="circle-wrap">
+        <div class="left-arrow transform -translate-x-[2px]"></div>
+      </button>
 
-    <div class="flex flex-wrap gap-3">
-      <div
-        v-for="photo in currentUser.photos"
-        :key="photo"
-        class="bg-white rounded-lg shadow p-2 w-64"
-      >
-        <img
-          :src="photo"
-          alt="avatar"
-          class="w-full object-cover rounded h-80"
-        />
+      <div class="flex flex-wrap gap-3">
+        <div
+          v-for="photo in currentUser.photos"
+          :key="photo"
+          class="bg-white rounded-lg shadow p-2 w-64"
+        >
+          <img
+            :src="photo"
+            alt="avatar"
+            class="w-full object-cover rounded h-80"
+          />
+        </div>
       </div>
-    </div>
-    <button type="button" @click="nextUser" class="circle-wrap">
-      <div class="right-arrow transform translate-x-[2px]"></div>
-    </button>
+
+      <button type="button" @click="nextUser" class="circle-wrap">
+        <div class="right-arrow transform translate-x-[2px]"></div>
+      </button>
+    </section>
+    <!-- 個人資訊頁面 -->
+    <section class="px-4 pt-4 w-full mt-4">
+      <button
+        @click="infoToggle"
+        type="button"
+        class="-mb-4 relative z-10 block mx-auto px-5 py-2 rounded-full font-semibold text-[#2c3e50] bg-[#f8f9fa] border border-[#2c3e50] shadow-md hover:bg-[#2c3e50] hover:text-white transition duration-300"
+      >
+        {{ infoBtnTxt }}
+      </button>
+      <transition name="fade-slide">
+        <div v-show="isShow" class="bg-[#fff3cd] -mt-5 relative z-0 p-5">
+          <div class="flex items-center">
+            <h3>{{ currentUser.name }}</h3>
+            <p class="font-semibold mx-2 text-[#ffb485]">
+              {{ currentUser.age }}
+            </p>
+            <p class="font-semibold">{{ currentUser.location }}</p>
+          </div>
+
+          <hr class="my-2" />
+          <h4 class="">關於我</h4>
+          <span class="personal-tag">
+            {{ currentUser.mbti }}
+          </span>
+          <span class="personal-tag">
+            {{ currentUser.zodiac }}
+          </span>
+          <h4>興趣</h4>
+          <span
+            v-for="interest in currentUser.interests"
+            :key="interest"
+            class="personal-tag"
+          >
+            {{ interest }}
+          </span>
+          <hr class="my-2" />
+          <h4>自我介紹</h4>
+          <p class="text-[#2c3e50]">
+            {{ currentUser.bio }}
+          </p>
+        </div>
+      </transition>
+    </section>
   </main>
-
-  <div>
-    <button
-      type="button"
-      class="block mx-auto my-3 px-5 py-2 rounded-full font-semibold text-[#2c3e50] bg-[#f8f9fa] border border-[#2c3e50] shadow-md hover:bg-[#2c3e50] hover:text-white transition duration-300"
-    >
-      Show More
-    </button>
-    <div class="bg-yellow-300">
-      <h3>徐老師{{ currentUser.name }}</h3>
-      <span class="flex gap-6 mb-4 text-base text-gray-600"
-        >年齡：{{ currentUser.age }}</span
-      >
-      <span class="mt-4 mb-1 text-[#2c3e50] font-semibold"
-        >星座：{{ currentUser.zodiac }}</span
-      >
-      <h3 class="my-1">關於我</h3>
-      <hr class="mb-4" />
-      <h3>興趣</h3>
-
-      <span
-        v-for="interest in currentUser.interests"
-        :key="interest"
-        class="font-semibold mx-1 text-white text-base bg-[#C0D7EC] px-3 py-1 rounded-s"
-      >
-        {{ interest }}
-      </span>
-
-      <h3>自我介紹</h3>
-      <hr class="mb-4" />
-      <p>
-        {{ currentUser.bio }}
-      </p>
-    </div>
-  </div>
 </template>
 
 <style scoped>
 h3 {
-  @apply text-2xl font-semibold text-[#2c3e50];
+  @apply text-2xl my-2 font-semibold text-[#2c3e50];
 }
 
-p {
-  @apply text-xl;
+h4 {
+  @apply text-xl my-2 font-semibold text-[#2c3e50];
+}
+
+.personal-tag {
+  @apply font-semibold mx-1 text-white text-base bg-[#C0D7EC] px-3 py-1 rounded-s my-1;
 }
 
 .left-arrow {
@@ -157,5 +187,22 @@ p {
   display: flex;
   align-items: center;
   justify-content: center;
+}
+
+.fade-slide-enter-active,
+.fade-slide-leave-active {
+  transition: all 0.3s ease;
+}
+
+.fade-slide-enter-from,
+.fade-slide-leave-to {
+  opacity: 0;
+  transform: translateY(-10px);
+}
+
+.fade-slide-enter-to,
+.fade-slide-leave-from {
+  opacity: 1;
+  transform: translateY(0);
 }
 </style>
