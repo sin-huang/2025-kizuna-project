d760e1f5dae16288a40341aeea718685f19ac873
HeidiSiao <heidi.siao@gmail.com>
2025-05-31 07:09:16 +0800

    chore: discard changes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,7 +9,7 @@ import ProfileView from "@/views/ProfileView.vue";
 import ChatRoomView from "@/views/ChatRoomView.vue";
 import MatchView from "@/views/MatchView.vue";
 import ActivityList from "@/components/ActivityList.vue";
-import EditProfileView from "../views/EditProfileView.vue";
+import EditProfileView from "@/views/EditProfileView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
