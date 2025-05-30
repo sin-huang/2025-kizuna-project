22e7c18fb59225b61b8d4097a4179bc98a43e1f1
HeidiSiao <heidi.siao@gmail.com>
2025-05-31 06:00:21 +0800

    feat: split components and add Like/Dislike/SuperLike buttons

---
+++ frontend/src/components/MatchBtn.vue
@@ -0,0 +1,87 @@
+<script setup>
+// 傳入對方的 userId，接著對對方 貼上喜歡/不喜歡的標籤 通知給父組件
+const { targetUser } = defineProps({
+  targetUser: {
+    type: Number,
+    required: true,
+  },
+});
+const emit = defineEmits(["like", "dislike"]);
+const likeHandler = () => {
+  emit("like", targetUser);
+};
+
+const dislikeHandler = () => {
+  emit("dislike", targetUser);
+};
+</script>
+
+<template>
+  <div class="flex items-center justify-center w-48 gap-16 px-2">
+    <button
+      type="button"
+      @click="dislikeHandler"
+      class="circle-wrap bg-[#c0d7ec] transform hover:scale-125"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        class="w-10 h-10 text-red-600"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    </button>
+    <button type="button" class="circle-wrap bg-[#f8f9fa] hover:bg-[#2c3e50]">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        class="w-10 h-10 text-pink-300"
+      >
+        <path
+          d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z"
+        />
+      </svg>
+    </button>
+
+    <button
+      type="button"
+      @click="likeHandler"
+      class="circle-wrap bg-[#c0d7ec] transform hover:scale-125"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        class="w-10 h-10 text-teal-700"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.circle-wrap {
+  /* 記得class另外補上背景色 */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
+  width: 3rem;
+  height: 3rem;
+  border-radius: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
