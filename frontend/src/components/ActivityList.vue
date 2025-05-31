2e675fc18c9c90cd92665ee14d952a9a1c27c415
Wenyu <Judy30058@gmail.com>
2025-05-31 16:54:17 +0800

    fix: activity.createdAt

--- frontend/src/components/ActivityList.vue
+++ frontend/src/components/ActivityList.vue
@@ -1,29 +1,26 @@
 <template>
-      <div
-        v-for="activity in activities"
-        :key="activity.id"
-      >
-        <h2>{{ activity.title }}</h2>
-        <p>{{ activity.location }}</p>
-        <p>{{ activity.description }}</p>
-        <p>建立者：{{ activity.createdBy }}</p>
-      </div>
+  <div 
+  v-for="activity in activities" :key="activity.id">
+    <h2>{{ activity.title }}</h2>
+    <p>{{ activity.location }}</p>
+    <p>{{ activity.description }}</p>
+    <p>建立者：{{ activity.createdBy }}</p>
+    <p>{{ activity.createdAt }}</p>
+  </div>
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
-import { useActivityStore } from '@/stores/activity.js'
-import { storeToRefs } from 'pinia'
+import { onMounted } from "vue";
+import { useActivityStore } from "@/stores/activity.js";
+import { storeToRefs } from "pinia";
 
-const store = useActivityStore()
-const { activities, loading, error } = storeToRefs(store)
-const { fetchActivities } = store
+const store = useActivityStore();
+const { activities, loading, error } = storeToRefs(store);
+const { fetchActivities } = store;
 
 onMounted(() => {
-  fetchActivities()
-})
-
+  fetchActivities();
+});
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
