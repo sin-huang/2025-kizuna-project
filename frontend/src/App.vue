84eefc6578c6361e52c3db78b33f6377dccb5e44
RU <c16033@mail.com>
2025-06-04 23:25:10 +0800

    feat(header): adjust navbar display logic based on login status

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,67 +14,8 @@ const handleLogout = () => {
 
 <template>
   <Header></Header>
-  <header class="text-white shadow-md bg-primary-100">
-    <nav class="flex justify-start">
-      <RouterLink to="/" class="p-6 text-lg transition hover:text-gray-300"
-        >首頁</RouterLink
-      >
-      <RouterLink
-        to="/product"
-        class="p-6 text-lg transition hover:text-gray-300"
-        >商品列表</RouterLink
-      >
-      <RouterLink to="/cart" class="p-6 text-lg transition hover:text-gray-300"
-        >購物車</RouterLink
-      >
-      <!-- 有token 代表已經登入 所以只剩登出按鈕 -->
-      <template v-if="store.accessToken">
-        <button
-          @click="handleLogout"
-          class="p-6 text-lg transition hover:text-gray-300"
-        >
-          登出
-        </button>
-        <div class="p-6 text-lg transition hover:text-gray-300">
-          目前登入帳號為 :
-          <!-- ml-2表示 margin-left加上0.5rem的間距 -->
-          <span class="ml-2">{{ store.username }}</span>
-        </div>
-        <!-- 要登入才能看到個人檔案頁面 -->
-        <RouterLink
-          to="/edit-profile"
-          class="p-6 text-lg transition hover:text-gray-300"
-          >編輯個人檔案區</RouterLink
-        >
-      </template>
 
-      <!-- 沒有token 請先登入或註冊新帳號 -->
-      <template v-else>
-        <RouterLink
-          to="/login"
-          class="p-6 text-lg transition hover:text-gray-300"
-          >登入</RouterLink
-        >
-        <RouterLink
-          to="/register"
-          class="p-6 text-lg transition hover:text-gray-300"
-          >註冊</RouterLink
-        >
-      </template>
-      <RouterLink to="/chat" class="p-6 text-lg transition hover:text-gray-300"
-        >聊天室</RouterLink
-      >
-      <RouterLink to="/match" class="p-6 text-lg transition hover:text-gray-300"
-        >配對池</RouterLink
-      >
-      <RouterLink to="/activities" class="p-6 text-lg transition hover:text-gray-300"
-        >活動</RouterLink
-      >
-    </nav>
-  </header>
-  <main class="max-w-[1000px] mx-auto mt-24">
-    <RouterView />
-  </main>
+  <main class="max-w-[1000px] mx-auto mt-24"></main>
 </template>
 
 <style></style>
--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,91 +1,3 @@
-<template>
-  <header class="text-white bg-[#7395BA] shadow-md">
-    <nav class="flex items-center justify-between px-6 py-4">
-      <!-- 左側 -->
-      <div class="flex items-center">
-        <RouterLink to="/" class="text-2xl italic font-light leading-none">
-          Kizuna
-        </RouterLink>
-      </div>
-
-      <!-- 中間 -->
-      <div class="flex justify-center flex-1 space-x-4">
-        <RouterLink
-          to="?"
-          class="p-4 text-lg leading-none transition hover:text-gray-300"
-        >
-          參與活動
-        </RouterLink>
-
-        <RouterLink
-          to="?"
-          class="p-4 text-lg leading-none transition hover:text-gray-300"
-        >
-          開始滑滑
-        </RouterLink>
-
-        <RouterLink
-          to="/product"
-          class="p-4 text-lg leading-none transition hover:text-gray-300"
-        >
-          購買禮物
-        </RouterLink>
-
-        <RouterLink
-          to="/product"
-          class="p-4 text-lg leading-none transition hover:text-gray-300"
-        >
-          解鎖升級
-        </RouterLink>
-      </div>
-
-      <!-- 右側 -->
-      <div class="flex items-center space-x-4">
-        <RouterLink
-          to="/cart"
-          class="text-lg leading-none transition hover:text-gray-300"
-        >
-          <font-awesome-icon :icon="['fas', 'cart-shopping']"size="1x" /> 購物車
-          
-        </RouterLink>
-
-        <div class="relative">
-          <div
-            class="flex items-center justify-center w-12 h-12 text-sm font-bold bg-white rounded-full text-[#7395BA] hover:bg-slate-300"
-            @click="toggleDropdown"
-          >
-          </div>
-          <div
-            v-if="isDropdownOpen"
-            class="absolute right-0 w-40 mt-2 overflow-hidden bg-white border border-gray-200 rounded-lg shadow-lg"
-          >
-                    <span class="flex items-center justify-center px-4 py-4 text-gray-500" > {{ store.username }}</span>
-            <a
-              href="#"
-              class="block px-4 py-4 text-gray-600 border-b border-gray-300 hover:bg-gray-100"
-            >
-              <font-awesome-icon :icon="['fas', 'user-pen']" /> 編輯個人檔案</a
-            >
-            <a
-              href="#"
-              class="block px-4 py-4 text-gray-600 border-b border-gray-300 hover:bg-gray-100"
-              ><font-awesome-icon :icon="['fas', 'star']" />   升級方案</a
-            >
-            <a
-              href="#"
-              class="block px-4 py-4 text-gray-600 border-b border-gray-300 hover:bg-gray-100"
-              @click="handleLogout"
-              ><font-awesome-icon :icon="['fas', 'right-to-bracket']" />  登出</a
-            >
-          </div>
-        </div>
-      </div>
-    </nav>
-  </header>
-  <main class="max-w-[1000px] mx-auto mt-24">
-    <RouterView />
-  </main>
-</template>
 <script setup>
 import { ref, onMounted, onUnmounted } from "vue";
 import { RouterLink, RouterView } from "vue-router";
@@ -124,4 +36,115 @@ onUnmounted(() => {
   window.removeEventListener("click", closeDropdown);
 });
 </script>
+
+<template>
+  <header class="text-white bg-[#7395BA] shadow-md">
+    <nav class="flex items-center justify-between px-6 py-4">
+      <!-- 左側 -->
+      <div class="flex items-center">
+        <RouterLink to="/" class="text-2xl italic font-light leading-none">
+          Kizuna
+        </RouterLink>
+      </div>
+
+      <!-- 中間 -->
+      <div class="flex justify-center flex-1 space-x-4">
+        <RouterLink
+          to="?"
+          class="p-4 text-lg leading-none transition hover:text-gray-300"
+        >
+          活動
+        </RouterLink>
+
+        <RouterLink
+          to="/match"
+          class="p-4 text-lg leading-none transition hover:text-gray-300"
+        >
+          配對池
+        </RouterLink>
+
+        <RouterLink
+          to="/product"
+          class="p-4 text-lg leading-none transition hover:text-gray-300"
+        >
+          商品列表
+        </RouterLink>
+
+        <RouterLink
+          to="/chat"
+          class="p-4 text-lg leading-none transition hover:text-gray-300"
+          >聊天室</RouterLink
+        >
+
+        <RouterLink
+          to="/cart"
+          class="p-4 text-lg leading-none transition hover:text-gray-300"
+        >
+          <font-awesome-icon :icon="['fas', 'cart-shopping']" size="1x" />
+          購物車
+        </RouterLink>
+      </div>
+
+      <!-- 右側 -->
+
+      <!-- 沒有token 請先登入或註冊新帳號 -->
+      <template v-if="!store.accessToken">
+        <RouterLink
+          to="/login"
+          class="p-4 text-lg leading-none transition hover:text-gray-300"
+          >登入</RouterLink
+        >
+        <RouterLink
+          to="/register"
+          class="p-4 text-lg leading-none transition hover:text-gray-300"
+          >註冊</RouterLink
+        >
+      </template>
+
+      <!-- 已登入顯示：帳號名稱 + 頭像選單 -->
+      <div class="flex items-center space-x-4" v-if="store.accessToken">
+        <div class="relative">
+          <div
+            class="flex items-center justify-center w-12 h-12 text-sm font-bold bg-white rounded-full text-[#7395BA] hover:bg-slate-300"
+            @click="toggleDropdown"
+          ></div>
+          <div
+            v-if="isDropdownOpen"
+            class="absolute right-0 w-40 mt-2 overflow-hidden bg-white border border-gray-200 rounded-lg shadow-lg"
+          >
+            <div
+              class="flex items-center justify-center px-4 py-4 text-gray-500"
+            >
+              目前登入帳號為 :
+              {{ store.username }}
+            </div>
+            <RouterLink
+              to="/edit-profile"
+              class="block px-4 py-4 text-gray-600 border-b border-gray-300 hover:bg-gray-100"
+              @click="isDropdownOpen = false"
+            >
+              <font-awesome-icon :icon="['fas', 'user-pen']" />
+              編輯個人檔案
+            </RouterLink>
+            <a
+              href="#"
+              class="block px-4 py-4 text-gray-600 border-b border-gray-300 hover:bg-gray-100"
+              ><font-awesome-icon :icon="['fas', 'star']" /> 升級方案</a
+            >
+            <a
+              href="#"
+              class="block px-4 py-4 text-gray-600 border-b border-gray-300 hover:bg-gray-100"
+              @click="handleLogout"
+              ><font-awesome-icon :icon="['fas', 'right-to-bracket']" /> 登出</a
+            >
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <main class="max-w-[1000px] mx-auto mt-24">
+    <RouterView />
+  </main>
+</template>
+
 <style></style>
