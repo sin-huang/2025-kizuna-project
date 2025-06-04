8342152974b81b22a50d9a4b0ad972556754417f
HeidiSiao <heidi.siao@gmail.com>
2025-06-04 12:25:22 +0800

    temp

--- backend/drizzle/0002_flowery_red_wolf.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "photos" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"image_url" varchar(255),
-	"image_key" varchar(255),
-	"uploaded_at" timestamp DEFAULT now()
-);
--- backend/drizzle/0003_odd_blink.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "photos" DROP COLUMN "uploaded_at";
